750569c9248d15e83b6aa8b5604f7b687057a5cb
Bridgit Donnelly <bridgit.donnelly@gmail.com>
2015-05-26 12:56:57 -0500

    Updated imputation method for academic data

--- pipeline/mcps_pipeline.py
+++ pipeline/mcps_pipeline.py
@@ -107,15 +107,27 @@ def process_data(df, variables):
 
     ## Fill missing school data -- use mean imputation for now
     school_vars = ['g6_school_id', 'g7_school_id', 'g8_school_id', 'g9_school_id', 'g10_school_id', 'g11_school_id', 'g12_school_id']
-    ml.replace_with_mean(df, school_vars)
+    #ml.replace_with_mean(df, school_vars)
 
     ## Fill missing grade and test score information -- use mean imputation for now
     grades_tests = ['g6_q1mpa', 'g6_q2mpa', 'g6_q3mpa', 'g6_q4mpa', 'g6_g6mapr','g7_q1mpa', 'g7_q2mpa', 'g7_q3mpa', 'g7_q4mpa', 'g7_g7mapr', 'g8_q1mpa', 'g8_q2mpa', 'g8_q3mpa', 'g8_q4mpa', 'g8_g8mapr', 'g9_q1mpa', 'g9_q2mpa', 'g9_q3mpa', 'g9_q4mpa', 'g9_g8mapr', 'g10_q1mpa', 'g10_q2mpa', 'g10_q3mpa', 'g10_q4mpa', 'g10_psatv', 'g10_psatm', 'g11_q1mpa', 'g11_q2mpa', 'g11_q3mpa', 'g11_q4mpa', 'g11_psatv', 'g11_psatm', 'g12_q1mpa', 'g12_q2mpa', 'g12_q3mpa', 'g12_q4mpa', 'g12_psatv', 'g12_psatm']
-    ml.replace_with_mean(df, grades_tests)
+    #ml.replace_with_mean(df, grades_tests)
 
     ## Fill missing MSAM data -- CAN'T FIGURE THIS ONE OUT
-    #msam_cols = ['g6_g6msam', 'g7_g7msam', 'g8_g8msam', 'g9_g9msam']
-    #ml.replace_with_value(df, msam_cols, [0,0,0,0])
+    msam_cols = ['g6_g6msam', 'g7_g7msam', 'g8_g8msam', 'g9_g8msam']
+    
+    #Recode existing data
+    for col in msam_cols:
+        for index, row in df.iterrows():
+            if row[col] == "Advanced":
+                df.ix[index, col] = 2
+            elif row[col] == "Proficient"
+                df.ix[index, col] = 1
+            elif row[col] == "Basic"
+                df.ix[index, col] = 0
+
+    # Replace missing
+    ml.replace_with_mean(df, msam_cols)
 
     ############################
     ## IMPUTE BEHAVIORAL DATA ##
@@ -125,7 +137,7 @@ def process_data(df, variables):
 
     ## Fill missing behavioral data -- use mean imputation for now
     behavioral_cols = ['g6_absrate', 'g6_nsusp','g7_absrate', 'g7_tardyr', 'g7_nsusp', 'g8_absrate', 'g8_tardyr', 'g8_nsusp', 'g9_absrate', 'g9_nsusp', 'g10_absrate', 'g10_nsusp', 'g11_absrate', 'g11_nsusp','g12_absrate', 'g12_nsusp']
-    ml.replace_with_mean(df, behavioral_cols)
+    #ml.replace_with_mean(df, behavioral_cols)
 
     ############################
     ## IMPUTE ENROLLMENT DATA ##
@@ -145,7 +157,7 @@ def process_data(df, variables):
 
     ## Fill missing dropout information with 0
     dropout_vars = ['g6_dropout', 'g7_dropout', 'g8_dropout', 'g9_dropout', 'g10_dropout', 'g11_dropout', 'g12_dropout']
-    ml.replace_with_value(df, dropout_vars, [0,0,0,0,0,0,0])
+    #ml.replace_with_value(df, dropout_vars, [0,0,0,0,0,0,0])
 
     #variables = list(df.columns.values)
     #print variables
