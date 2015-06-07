9d8953a4826aab4e3bcfed13c2a6d589f672abae
Christine Cook <mac@Macs-MacBook-Pro-94.local>
2015-06-07 14:08:08 -0500

    cmon3

--- pipeline/cook-pipeline-2.py
+++ pipeline/cook-pipeline-2.py
@@ -254,7 +254,7 @@ def eval_clfs(y_pred, y_data, evals, classifier, classifier_name, poss_times, y_
 
 def clf_and_evals(list_of_inputs):
     print len(list_of_inputs)
-    print list_of_inputs[0]
+    print list_of_inputs[0][0]
     classifer_name = list_of_inputs[0]
     classifier_class = list_of_inputs[1]
     x_data = list_of_inputs[2]
