0692ac76106c5a5f9cacd2a59b1b1db769d6149f
Christine Cook <mac@wireless-230-97.uchicago.edu>
2015-06-08 13:42:59 -0500

    pushhh

--- pipeline/cook-pipeline-3.py
+++ pipeline/cook-pipeline-3.py
@@ -263,7 +263,7 @@ def main():
     #start k-fold
     for x in range(0, 1):
         print "Split: " + str(x)
-        train_data, test_data = train_test_split(data, test_size=.2)
+        train_data, test_data = train_test_split(data, test_size=.2) 
 
         #conditional mean imputation
         #train_data = imputeConditionalMean(train_data, DV)
