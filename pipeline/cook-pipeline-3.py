51888050656d4967b2e91a8a19c9fcae5b4bc35a
Christine Cook <mac@wireless-230-97.uchicago.edu>
2015-06-08 13:42:27 -0500

    stuff6

--- pipeline/cook-pipeline-3.py
+++ pipeline/cook-pipeline-3.py
@@ -258,7 +258,7 @@ def main():
 
     # define parameters
     names = ["Nearest Neighbors", "Linear SVM", "Decision Tree", "Random Forest", "AdaBoost", "Linear Regression", "Bagging", "Logistic Regression", "Stochastic Gradient Descent"]
-    classifiers = [KNeighborsClassifier(3), LinearSVC(C=0.025, class_weight = {0: 1, 1:10}), DecisionTreeClassifier(max_depth=5), RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1, class_weight = {0: 1, 1:10}), AdaBoostClassifier(), linear_model.LinearRegression(), BaggingClassifier(), linear_model.LogisticRegression(), SGDClassifier(loss="hinge", penalty="l2")]
+    classifiers = [KNeighborsClassifier(3), LinearSVC(C=0.025, class_weight = {0: 1, 1:10}), DecisionTreeClassifier(max_depth=5), RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1, class_weight = {0: 1, 1:10}), AdaBoostClassifier(), linear_model.LinearRegression(class_weight = {0: 1, 1:10}), BaggingClassifier(), linear_model.LogisticRegression(), SGDClassifier(loss="hinge", penalty="l2")]
 
     #start k-fold
     for x in range(0, 1):
